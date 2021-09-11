0eb129e3ee1ae1c0c0898e095787b680aeb50b1c
LongbeoRepo <koojin1302@gmail.com>
2021-09-11 16:58:07 +0700

    Create a new chanle.cpp

---
+++ chanle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n; //con tro vao n: nhap gia tri input rooif gan vao n
+	if(n%2==0){
+		cout<<"n la so chan";
+	}	
+	else{
+		cout<<"n la so le";
+	}
+	return 0;
+}
